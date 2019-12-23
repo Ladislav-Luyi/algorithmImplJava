c6eb2afb9dc90aeb6e1ecae44fde3499e4d8aafc
Ladislav-Luyi <56082297+Ladislav-Luyi@users.noreply.github.com>
2019-12-23 15:01:03 +0100

    Create IterativeLinearSearch.java

---
+++ IterativeLinearSearch.java
@@ -0,0 +1,23 @@
+
+public class IterativeLinearSearch {
+
+    public static void main(String[] args) throws InterruptedException {
+        int[] arrayForSearching = {15,30,0,7,8,9,13,12,10};
+        int targetValue = 10;
+
+        System.out.println("Target value is on index: " + search(arrayForSearching, targetValue));
+
+    }
+
+    public static int search(int[] array,int targetValue){
+        int index = -1;
+
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == targetValue) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+}
