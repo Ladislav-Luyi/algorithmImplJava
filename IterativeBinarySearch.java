07943a95deb042c0d37c43a4a95186e840d87cfa
Ladislav-Luyi <ladislav.szabo999@gmail.com>
2020-05-06 18:17:26 +0200

    some changes

--- IterativeBinarySearch.java
+++ IterativeBinarySearch.java
@@ -1,13 +1,13 @@
 public class IterativeBinarySearch {
     public static void main(String[] args) {
         int[] arrayForSearch = {1,2,3,4,5,6,7,8,9,10,11};
-        int targetNumber = 30;
+        int targetNumber = 8;
         int low = 0;
         int high = arrayForSearch.length -1;
         System.out.println( search(arrayForSearch, low, high, targetNumber) );
     }
 
-    public static int search(int[] array, int low, int high, int target){
+    private static int search(int[] array, int low, int high, int target){
         int index = -1;
         while (low <= high){
             int mid = (low + high) / 2;
